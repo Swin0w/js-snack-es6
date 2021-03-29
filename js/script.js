1990c8b24bceaa6182d1135e78b5d72655c45515
Simone <marzolla.istruzione@gmail.com>
2021-03-29 10:46:36 +0200

    2,3

--- js/script.js
+++ js/script.js
@@ -2,15 +2,81 @@
 seguenti proprietà: nome, cognome e età. Stampare a
 schermo attraverso il for in tutte le proprietà. */
 function es1 () {
-  var studente = {
+  var student = {
     'nome': 'Mario',
     'cognome': 'Rossi',
     'age': '30'
   };
 
-  for (var i in studente) {
-    var value = studente[key];
-    console.log(i + ":" + value);
+  for (var i in student) {
+    var value = student[i];
+    console.log(i + ": " + value);
   }
 }
+console.log("-----------------");
 es1();
+console.log("-----------------");
+console.log(" ");
+function es2() {
+/* Creare un array di oggetti di studenti. Ciclare su tutti gli
+studenti e stampare per ognuno nome e cognome */
+  var students = [
+    {
+      'nome': 'Mario',
+      'cognome': 'Rossi',
+      'age': '20'
+    },
+    {
+      'nome': 'Luca',
+      'cognome': 'Prassi',
+      'age': '20'
+    },
+    {
+      'nome': 'Pino',
+      'cognome': 'Daniele',
+      'age': '20'
+    },
+    {
+      'nome': 'Giulio',
+      'cognome': 'DiNapoli',
+      'age': '20'
+    }
+  ];
+
+  for (var i = 0; i < students.length; i++) {
+    console.log(students[i]['nome'], students[i]['cognome']);
+  }
+}
+console.log("-----------------");
+es2();
+console.log("-----------------");
+
+function es3() {
+  /* Dare la possibilità all'utente attraverso 3 prompt di
+     aggiungere un nuovo oggetto studente inserendo
+     nell'ordine: nome, cognome e età.*/
+
+     let addName = prompt("Dammi un nome:");
+     let addSurname = prompt("Dammi un cognome:");
+     let addAge = parseInt(prompt("Dammi un'età:"));
+
+     let students = [
+       {
+         'nome': 'Beppe',
+         'cognome': 'Licorni',
+         'age': 22
+       },
+       {
+         'nome': addName,
+         'cognome': addSurname,
+         'age': addAge
+       }
+     ];
+     for (var i = 0; i < students.length; i++) {
+       console.log(students[i]['nome'], students[i]['cognome']);
+     }
+
+}
+console.log("---------------");
+es3();
+console.log("---------------");
